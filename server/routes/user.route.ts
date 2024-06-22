c3fcb1df8c0ab20434ae946c6cad501edd248a82
Hemant Kumar <hemant210433@gmail.com>
2024-06-22 13:22:02 +0530

    create authorizeRoles in auth.ts

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { activateUser, loginUser,logoutUser , registrationonUser } from "../controllers/user.controller";
-import { isAutheticated } from "../middleware/auth";
+import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationonUser);
 userRouter.post('/activate-user',activateUser);
 userRouter.post('/login',loginUser);
-userRouter.get('/logout',isAutheticated,logoutUser);
+userRouter.get('/logout',isAutheticated,authorizeRoles("admin"),logoutUser);
 
 export default userRouter;
