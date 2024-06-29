341ad875af7f61f3d3a63b74229060f1f047de6c
Hemant Kumar <hemant210433@gmail.com>
2024-06-29 17:46:43 +0530

    add social auth router and controller

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -9,6 +9,7 @@ import path from "path";
 import sendMail from "../utils/sendMail";
 import { accessTokenOptions, refreshTokenOptions, sendToken } from "../utils/jwt";
 import { redis } from "../utils/redis";
+import { getUserById } from "../services/user.service";
 
 // register user
 
@@ -136,14 +137,14 @@ export const logoutUser = catchAsyncError(
     async (req: Request, res: Response, next: NextFunction) => {
         try {
             res.cookie("access_token", "", { maxAge: 1 });
-            res.cookie("refresh_token", "", { maxAge: 1 });        
+            res.cookie("refresh_token", "", { maxAge: 1 });
             const userId = req.user?._id;
             console.log(req.user);
 
             if (!userId) {
                 return next(new ErrorHandler("User ID not found", 400));
             }
-            const userIdAsString = String(userId); 
+            const userIdAsString = String(userId);
             await redis.del(userIdAsString);
             res.status(200).json({
                 success: true,
@@ -157,57 +158,95 @@ export const logoutUser = catchAsyncError(
 // update access token
 export const updateAccessToken = catchAsyncError(
     async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const refresh_token = req.cookies.refresh_token as string;
-        const decoded = jwt.verify(
-          refresh_token,
-          process.env.REFRESH_TOKEN as string
-        ) as JwtPayload;
-  
-        const message = "Could not refresh token";
-        if (!decoded) {
-          return next(new ErrorHandler(message, 400));
+        try {
+            const refresh_token = req.cookies.refresh_token as string;
+            const decoded = jwt.verify(
+                refresh_token,
+                process.env.REFRESH_TOKEN as string
+            ) as JwtPayload;
+
+            const message = "Could not refresh token";
+            if (!decoded) {
+                return next(new ErrorHandler(message, 400));
+            }
+            const session = await redis.get(decoded.id as string);
+
+            if (!session) {
+                return next(
+                    new ErrorHandler("Please login for access this resources!", 400)
+                );
+            }
+
+            const user = JSON.parse(session);
+
+            const accessToken = jwt.sign(
+                { id: user._id },
+                process.env.ACCESS_TOKEN as string,
+                {
+                    expiresIn: "5m",
+                }
+            );
+
+            const refreshToken = jwt.sign(
+                { id: user._id },
+                process.env.REFRESH_TOKEN as string,
+                {
+                    expiresIn: "3d",
+                }
+            );
+
+            // req.user = user;
+
+            res.cookie("access_token", accessToken, accessTokenOptions);
+            res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+            res.status(200).json({
+                status: "sucess",
+                accessToken,
+            })
+
+            // await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7days
+
+            // return next();
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message, 400));
         }
-        const session = await redis.get(decoded.id as string);
-           
-        if (!session) {
-          return next(
-            new ErrorHandler("Please login for access this resources!", 400)
-          );
-        }
-        
-        const user = JSON.parse(session);
-  
-        const accessToken = jwt.sign(
-          { id: user._id },
-          process.env.ACCESS_TOKEN as string,
-          {
-            expiresIn: "5m",
-          }
-        );
-  
-        const refreshToken = jwt.sign(
-          { id: user._id },
-          process.env.REFRESH_TOKEN as string,
-          {
-            expiresIn: "3d",
-          }
-        );
-  
-        // req.user = user;
-  
-        res.cookie("access_token", accessToken, accessTokenOptions);
-        res.cookie("refresh_token", refreshToken, refreshTokenOptions);
-        res.status(200).json({
-            status:"sucess",
-            accessToken,
-        })
-  
-        // await redis.set(user._id, JSON.stringify(user), "EX", 604800); // 7days
-  
-        // return next();
-      } catch (error: any) {
-        return next(new ErrorHandler(error.message, 400));
-      }
     }
-  );
+);
+
+// get user information
+export const getUserInfo = catchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const userId = req.user?._id as string | undefined;
+            if (!userId) {
+                return next(new ErrorHandler("User ID not found", 400));
+            }
+            getUserById(userId, res);
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message, 400));
+        }
+    }
+);
+interface ISocialAuthBody {
+    email: string;
+    name: string;
+    avatar: string;
+}
+
+// social auth
+export const socialAuth = catchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { email, name, avatar } = req.body as ISocialAuthBody;
+            const user = await userModel.findOne({ email });
+            if (!user) {
+                const newUser = await userModel.create({ email, name, avatar });
+                sendToken(newUser, 200, res);
+            } else {
+                sendToken(user, 200, res);
+            }
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message, 400));
+        }
+    }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, loginUser,logoutUser , registrationonUser, updateAccessToken } from "../controllers/user.controller";
+import { activateUser, getUserInfo, loginUser,logoutUser , registrationonUser, socialAuth, updateAccessToken } from "../controllers/user.controller";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const userRouter = express.Router();
 
@@ -8,5 +8,7 @@ userRouter.post('/activate-user',activateUser);
 userRouter.post('/login',loginUser);
 userRouter.get('/logout',isAutheticated,authorizeRoles("admin"),logoutUser);
 userRouter.get("/refresh",updateAccessToken);
+userRouter.get("/me", isAutheticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
 
 export default userRouter;
