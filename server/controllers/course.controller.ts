72fca479cf70abb16685eafb1b1f91afdd83b706
Hemant Kumar <hemant210433@gmail.com>
2024-09-08 10:29:02 +0530

    create getCoursesByUser and route -- only for valid user

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -120,4 +120,27 @@ export const getAllCourses = catchAsyncError(
     }
   }
 );
-
+export const getCourseByUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userCourseList = req.user?.courses;
+      const courseId = req.params.id;
+      const courseExists = userCourseList?.find(
+        (course: any) => course._id.toString() === courseId.toString()
+      );
+      if (!courseExists) {
+        return next(
+          new ErrorHandler("You are not eligible to access this course", 404)
+        );
+      }
+      const course = await CourseModel.findById(courseId);
+      const content = course?.courseData;
+      res.status(200).json({
+        success: true,
+        content,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {uploadCourse,editCourse,getSingleCourse,getAllCourses } from "../controllers/course.controller";
+import {uploadCourse,editCourse,getSingleCourse,getAllCourses,getCourseByUser } from "../controllers/course.controller";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const courseRouter = express.Router();
 courseRouter.post(
@@ -22,5 +22,6 @@ courseRouter.post(
     "/get-courses",
     getAllCourses
   );
+  courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
 
 export default courseRouter;
