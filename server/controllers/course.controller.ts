9ab58550e50f1a89a2c3c90a08c453924a2e2337
Hemant Kumar <hemant210433@gmail.com>
2024-09-09 17:48:19 +0530

    create add-question route and controller

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -5,6 +5,7 @@ import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
 import { redis } from "../utils/redis";
+import mongoose from "mongoose";
 export const uploadCourse = catchAsyncError(
     async (req: Request, res: Response, next: NextFunction) => {
       try {
@@ -138,6 +139,42 @@ export const getCourseByUser = catchAsyncError(
       res.status(200).json({
         success: true,
         content,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+interface IAddQuestionData {
+  question: string;
+  courseId: string;
+  contentId: string;
+}
+export const addQuestion = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { question, courseId, contentId }: IAddQuestionData = req.body;
+      const course = await CourseModel.findById(courseId);
+
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+      const couseContent = course?.courseData?.find((item: any) =>
+        item._id.equals(contentId)
+      );
+      if (!couseContent) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+      const newQuestion: any = {
+        user: req.user,
+        question,
+        questionReplies: [],
+      };
+      couseContent.questions.push(newQuestion);
+      await course?.save();
+      res.status(200).json({
+        success: true,
+        course,
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {uploadCourse,editCourse,getSingleCourse,getAllCourses,getCourseByUser } from "../controllers/course.controller";
+import {uploadCourse,editCourse,getSingleCourse,getAllCourses,getCourseByUser,addQuestion} from "../controllers/course.controller";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const courseRouter = express.Router();
 courseRouter.post(
@@ -23,5 +23,6 @@ courseRouter.post(
     getAllCourses
   );
   courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
+  courseRouter.put("/add-question", isAutheticated, addQuestion);
 
 export default courseRouter;
