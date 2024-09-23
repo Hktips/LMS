31989eda2bb19706303298a50e2880bba0edbaf1
Hemant Kumar <hemant210433@gmail.com>
2024-09-23 18:28:01 +0530

    create question-reply file inside mails folder

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -24,6 +24,6 @@ courseRouter.post(
   );
   courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
   courseRouter.put("/add-question", isAutheticated, addQuestion);
-  courseRouter.put("/add-answer", isAutheticated, addAnwser);
+  courseRouter.put("/add-answer", isAutheticated, addAnwser); 
 
 export default courseRouter;
